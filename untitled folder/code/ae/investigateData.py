64d7d7c9302d7de831e4c5754c2e67c938d73fdf
zhecan wang <zhecanwang@zhecans-MacBook-Pro.local>
2016-11-13 10:54:01 -0500

    add ade code

---
+++ untitled folder/code/ae/investigateData.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pickle
+import matplotlib.pyplot as plt
+import numpy as np
+
+dataDir = "../data/"
+output = np.load(dataDir + "output.npy")
+print output.shape
+# print output[0]
+# x = output[0]
+# x_normed = x + np.amax(-x)
+# print(x_normed)
+
+testLabels = pickle.load( open( dataDir + "yTest.p", "rb" ) )[:1000]
+print testLabels.shape
+print testLabels[0]
+
+
+plt.hist(output[0])
+plt.hist(testLabels[0])
+
+# plt.title("Gaussian Histogram")
+# plt.xlabel("Value")
+# plt.ylabel("Frequency")
+plt.show()
